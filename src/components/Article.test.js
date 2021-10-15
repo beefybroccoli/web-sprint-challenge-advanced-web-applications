cbee12764d7394942ea1012417d462ea1271c672
beefybroccoli <tedkimemail@yahoo.com>
2021-10-15 07:28:57 -0700

    ...milestone-6

--- src/components/Article.test.js
+++ src/components/Article.test.js
@@ -1,13 +1,12 @@
-import React from 'react';
-import '@testing-library/jest-dom';
+import React from "react";
+import "@testing-library/jest-dom";
 
-import userEvent from '@testing-library/user-event';
-import MutationObserver from 'mutationobserver-shim';
+import userEvent from "@testing-library/user-event";
+import MutationObserver from "mutationobserver-shim";
 
-import Article from './Article';
+import Article from "./Article";
 
-test('renders component without errors', ()=> {
-});
+test("renders component without errors", () => {});
 
 // test('renders headline, author from the article when passed in through props', ()=> {
 // });
